c40bb1de2a8b664e07aab8bddf4ffa01daba27c1
Moon <Wellynton.souza@bandtec.com.br>
2020-09-10 15:31:25 -0300

    Tratamentos de erros feitos

--- projeto-individual-wellynton-dias/src/main/java/com/wellynton/projetoindividualwellyntondias/MonstroDeDueloController.java
+++ projeto-individual-wellynton-dias/src/main/java/com/wellynton/projetoindividualwellyntondias/MonstroDeDueloController.java
@@ -1,94 +1,153 @@
 package com.wellynton.projetoindividualwellyntondias;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/mds")
 public class MonstroDeDueloController {
 
-    private List<Monstros> monstros = new ArrayList<>();
-    private List<Dragoes> monstrosDragoes = new ArrayList<>();
-    private List<Magos> monstrosMagos = new ArrayList<>();
-    private List<Cavaleiro> monstrosCavaleiros = new ArrayList<>();
+     private List<Monstros> monstros = new ArrayList<>();
 
     @GetMapping("/monstros")
-    public List<Monstros> getMonstros(){
-        return monstros;
+    public ResponseEntity getMonstros(){
+        if (!monstros.isEmpty()){
+          return ResponseEntity.ok(monstros);
+        }else{
+            return ResponseEntity.noContent().build();
+        }
     }
 
     @GetMapping("/monstros/recuperar/{id}")
-    public Monstros getAllMonstros(@PathVariable int id){
+    public ResponseEntity getAllMonstros(@PathVariable int id){
 
-        return monstros.get(id-1);
+        if (!monstros.isEmpty()){
 
+            return ResponseEntity.ok(monstros.get(id-1));
+
+        }else {
+
+            return ResponseEntity.notFound().build();
+
+        }
     }
 
     @DeleteMapping("/monstros/excluir/{id}")
-    public void excluirMonstros(@PathVariable int id){
-        monstros.remove(id-1);
+    public ResponseEntity excluirMonstros(@PathVariable int id){
+       if (monstros.size() >= id){
+
+           monstros.remove(id-1);
+           return ResponseEntity.ok().build();
+
+       }else {
+
+           return ResponseEntity.notFound().build();
+
+       }
     }
 
     @PostMapping("/adcionarMonstros/dragoes")
-    public void adcionarDragao(@RequestBody Dragoes dragoes){
+    public ResponseEntity adcionarDragao(@RequestBody Dragoes dragoes){
 
         monstros.add(dragoes);
-        monstrosDragoes.add(dragoes);
+
+        return ResponseEntity.created(null).build();
 
     }
 
-    @GetMapping("/monstros/recuperarDragao/{id}")
-    public Dragoes getDragao(@PathVariable int id){
+    @GetMapping("/monstros/recuperarDragoes")
+    public ResponseEntity getRecuperarDragoes(){
 
-        return monstrosDragoes.get(id-1);
+     List<Monstros> monstrosDragoes = monstros.stream()
+             .filter(monstros1 -> monstros1.getTipo().equals("Dragão")).collect(Collectors.toList());
 
+     if (!monstrosDragoes.isEmpty()){
+         return ResponseEntity.ok(monstrosDragoes);
+     } else {
+         return ResponseEntity.noContent().build();
+     }
     }
 
-    @DeleteMapping("/monstros/excluir/dragoes/{id}")
-    public void excluirDragoes(@PathVariable int id){
-        monstrosDragoes.remove(id-1);
+    @GetMapping("/monstros/recuperarDragoesEspecificos/{id}")
+    public ResponseEntity recuperarDragoesEspecificos(@PathVariable int id){
+        List<Monstros> monstrosDragoes = monstros.stream()
+                .filter(monstros1 -> monstros1.getTipo().equals("Dragão")).collect(Collectors.toList());
+        if (!monstrosDragoes.isEmpty()){
+            return ResponseEntity.ok(monstrosDragoes.get(id-1));
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping("/adcionarMonstros/magos")
-    public void adcionarMago(@RequestBody Magos magos){
+    public ResponseEntity adcionarMago(@RequestBody Magos magos){
 
         monstros.add(magos);
-        monstrosMagos.add(magos);
-    }
 
-
-    @GetMapping("/monstros/recuperarMagos/{id}")
-    public Magos getMagos(@PathVariable int id){
-
-        return monstrosMagos.get(id-1);
+        return ResponseEntity.created(null).build();
 
     }
 
-    @DeleteMapping("/monstros/excluir/magos/{id}")
-    public void excluirMagos(@PathVariable int id){
-        monstrosMagos.remove(id-1);
+
+    @GetMapping("/monstros/recuperarMagos")
+    public ResponseEntity getMagos() {
+        List<Monstros> monstrosMagos = monstros.stream()
+                .filter(monstros1 -> monstros1.getTipo().equals("Mago")).collect(Collectors.toList());
+
+        if (monstrosMagos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(monstrosMagos);
+        }
+    }
+
+    @GetMapping("/monstros/recuperarMagosEspecificos/{id}")
+    public ResponseEntity getMagosEspecificos(@PathVariable int id) {
+        List<Monstros> monstrosMagos = monstros.stream()
+                .filter(monstros1 -> monstros1.getTipo().equals("Mago")).collect(Collectors.toList());
+
+        if (monstrosMagos.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(monstrosMagos.get(id-1));
+        }
     }
 
     @PostMapping("/adcionarMonstros/cavaleiros")
-    public void adcionarCavaleiro(@RequestBody Cavaleiro cavaleiros){
+    public ResponseEntity adcionarCavaleiro(@RequestBody Cavaleiro cavaleiros){
 
         monstros.add(cavaleiros);
-        monstrosCavaleiros.add(cavaleiros);
 
+        return ResponseEntity.created(null).build();
     }
 
-    @GetMapping("/monstros/recuperarCavaleiros/{id}")
-    public Cavaleiro getCavaleiros(@PathVariable int id){
+    @GetMapping("/monstros/recuperarCavaleiros")
+    public ResponseEntity getCavaleiros() {
 
-        return monstrosCavaleiros.get(id-1);
+        List<Monstros> monstrosCavaleiros = monstros.stream()
+                .filter(monstro -> monstro.getTipo().equals("Cavaleiro")).collect(Collectors.toList());
 
+        if (monstrosCavaleiros.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(monstrosCavaleiros);
+        }
     }
 
-    @DeleteMapping("/monstros/excluir/cavaleiros/{id}")
-    public void excluirCavaleiros(@PathVariable int id){
-        monstrosCavaleiros.remove(id-1);
-    }
+        @GetMapping("/monstros/recuperarCavaleirosEspecificos/{id}")
+        public ResponseEntity getCavaleirosEspecificos(@PathVariable int id){
 
+            List<Monstros> monstrosCavaleirosEspecificos = monstros.stream()
+                    .filter(monstro -> monstro.getTipo().equals("Cavaleiro")).collect(Collectors.toList());
+
+            if (monstrosCavaleirosEspecificos.isEmpty()){
+                return ResponseEntity.notFound().build();
+            }else {
+                return ResponseEntity.ok(monstrosCavaleirosEspecificos.get(id-1));
+            }
+        }
 }
